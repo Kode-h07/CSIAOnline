d2164998ced527dafce513c4f4d653fda42c1867
Oystersaregoated <jakeko0912@gmail.com>
2024-05-31 13:33:31 +0900

    urlfix

--- CSIAOnline/yaja/urls.py
+++ CSIAOnline/yaja/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 from yaja import views
 
-urlpatterns = [
-    path("yaja/", views.yaja_view, name="yaja"),
-    path("yaja/schedule", views.yajaSchedule_view, name="schedule"),
-]
+urlpatterns = [path("yaja/", views.yaja_view, name="yaja")]
