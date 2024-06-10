5eb64a0406cc5021db4c37fbcd3c5743e17f1009
IamEunjae <84075869+IamEunjae@users.noreply.github.com>
2024-06-10 22:18:36 +0900

    minor bug fixes
    Co-authored-by: Oystersaregoated <Kode-h07@users.noreply.github.com>

--- CSIAOnline/yaja/management/commands/update_google_sheet.py
+++ CSIAOnline/yaja/management/commands/update_google_sheet.py
@@ -3,10 +3,11 @@ import datetime
 from ...models import Monday, Tuesday, Wednesday, Thursday  # Update with your app name
 from django.core.management.base import BaseCommand
 
-GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyy7dyS1N4U657QY_j_zfgS38q1m1zUY2DeXpECE5oPoqPxImsn_OfRNfnCBizBV9AQ/exec'
+GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxehpePFzkQbXFiBuXnW6ObAFOFf4yHd2Rpfe5dU_LQWLtvirJDlNXj19iKGkm4I1_kPg/exec'
+
 def fetch_schedule():
     day_of_week = datetime.datetime.today().weekday()
-    if (day_of_week  == 0 or day_of_week == 4 or day_of_week == 5 or day_of_week==6):
+    if day_of_week in [0, 4, 5, 6]:
         schedules = Monday.objects.all()
     elif day_of_week == 1:
         schedules = Tuesday.objects.all()
@@ -26,7 +27,6 @@ def update_google_sheet():
     
     updates = []
     for schedule in schedules:
-
         updates.append({
             'student_id': schedule.student_id,
             'period1': schedule.period1,
@@ -37,14 +37,11 @@ def update_google_sheet():
 
     response = requests.post(GOOGLE_APPS_SCRIPT_URL, json=updates)
     if response.status_code == 200:
-        print(response)
         print("Google Sheet updated successfully.")
     else:
         print("Failed to update Google Sheet.", response.text)
 
-
 class Command(BaseCommand):
-    print("command called on update_google_sheet.py")
     help = "Resets user schedules to default values every Friday"
 
     def handle(self, *args, **kwargs):
